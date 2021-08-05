99ef779fad6d33145f00aadfb3f20dc65b1456e6
Ashar Farooq <ashar744@gmail.com>
2021-08-05 19:07:25 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from "react";
+import './App.css';
+
+function App() {
+
+  const [breaches, setBreaches] = useState();
+  const [dataTypes, setDataTypes] = useState();
+
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
