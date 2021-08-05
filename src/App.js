14b954a5fb5f7f7e0cf43e62d199725ec2cefa4f
Ashar Farooq <ashar744@gmail.com>
2021-08-05 19:52:15 -0400

    refactored useffects

--- src/App.js
+++ src/App.js
@@ -8,13 +8,31 @@ import './App.css';
 
 function App() {
 
+  const [breaches, setBreaches] = useState();
+  const [dataTypes, setDataTypes] = useState([]);
+
+useEffect(() => {
+  fetch("https://haveibeenpwned.com/api/v3/dataclasses")
+  .then(response => response.json())
+  .then(data => setDataTypes(data))
+  },[]);
+
+  useEffect(() => {
+  fetch("https://haveibeenpwned.com/api/v3/breaches")
+  .then(response => response.json())
+  .then(data => setBreaches(data))
+  },[]);
+
+
   return (
     <div className="App">
         <BrowserRouter>
           <Switch>
             <Route path="/" exact component={Home} />
             <Route path="/breaches" exact component={Breaches}/>
-            <Route path="/datatypes" exact component={DataTypes}/>
+            <Route path="/datatypes" exact   render={(props) => (
+    <DataTypes {...props} dataTypes={dataTypes} />
+  )}/>
             >
           </Switch>
         </BrowserRouter>
--- src/pages/dataTypes.js
+++ src/pages/dataTypes.js
@@ -2,18 +2,29 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 
-function DataTypes() {
+function DataTypes(props) {
 
-const [dataTypes, setDataTypes] = useState();
-
-useEffect(() => {
-  fetch("https://haveibeenpwned.com/api/v3/dataclasses")
-  .then(response => response.json())
-  .then(data => setDataTypes(data))
-  },[]);
   return (
         <>
-        <h1>{dataTypes}</h1>
+
+        {props.dataTypes.map(each => {
+          return (
+            <div>
+            <h1>{each}</h1>
+            </div>
+          )
+        })}
+        {/* {dataTypes.forEach((each_type) => { 
+          return (<h1>each_type</h1>)
+          }
+          )} */}
+        
+        {/* <h1>{
+        
+        dataTypes.forEach(each_type => {
+          return (<h1>each_type</h1>)
+        })
+        }</h1> */}
         </>
   );
 }
