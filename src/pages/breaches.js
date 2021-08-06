e832ddbc996deb5deacad22948ddb8c29e5cc50b
Ashar Farooq <ashar744@gmail.com>
2021-08-05 20:08:03 -0400

    added basic card

--- src/pages/breaches.js
+++ src/pages/breaches.js
@@ -1,16 +1,31 @@
-import React, {useState, useEffect} from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 
-function Breaches() {
-
-
-
+function Breaches({ breaches }) {
   return (
-        <>
-        <h1></h1>
-        </>
+    <>
+      {breaches.map((each_breach) => {
+        return (
+          <Card variant="outlined">
+            <CardContent>
+              <h2 class="text-danger">
+                {each_breach["Name"]} {each_breach["Domain"]}
+              </h2>
+
+              {/* <h6>Capital: {each_breach.capital}</h6>
+
+          <h6>Phone Extension: {each_breach.phone} </h6>
+
+          <h6>Country Code: {each_breach.code}</h6>
+
+          <h6>Currency: {each_breach.currency}</h6> */}
+            </CardContent>
+          </Card>
+        );
+      })}
+      }
+    </>
   );
 }
 
