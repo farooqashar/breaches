f0f3401dc35455b9d4f06a922e3f896d6d71a1f4
Ashar Farooq <ashar744@gmail.com>
2021-08-05 20:19:16 -0400

    home page cleanup

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,32 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-
-
 function Home() {
-
   return (
-        <>
-        <h1>Breaches</h1>
-      <Link to="/dataTypes">
-        <button class="btn btn-primary">Check Out Different Breachable Data Types</button>
-      </Link>
+    <>
+      <h1>Breaches</h1>
 
+      <p className="text-info container">
+        This is a simple webpage designed to feature information about various
+        breaches of different entities.
+      </p>
+
+      <div className="container">
+        <Link to="/dataTypes">
+          <button className="btn btn-primary">
+            Check Out Different Breachable Data Types
+          </button>
+        </Link>
+      </div>
+      <br />
+      <div className="container">
         <Link to="/breaches">
-        <button class="btn btn-primary">Check Out Various Breaches</button>
-      </Link>
-        </>
+          <button className="btn btn-primary">
+            Check Out Various Breaches
+          </button>
+        </Link>
+      </div>
+    </>
   );
 }
 
